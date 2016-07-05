ffa594efe29039b137f793c6371eb76b44899da3
ruxandrafed <ruxandra.fediuc@gmail.com>
2016-07-04 19:32:21 -0700

    Add button to add notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,31 +2,36 @@ import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Jump around'
-  }
-]
-
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
   render() {
+    const notes = this.state.notes;
     return (
       <div>
+        <button className="add-note" onClick={this.addNote}>+</button>
         <Notes items={notes} />
       </div>
     );
+  }
+  addNote() {
+    console.log('add note');
   }
 }
